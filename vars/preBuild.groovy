53c2004530f79857bddf620cd53171b1d4fa2bae
3dnagar <106944909+3dnagar@users.noreply.github.com>
2026-01-04 22:03:55 +0530

    Delete vars/preBuild.groovy

--- vars/preBuild.groovy
+++
@@ -1,8 +0,0 @@
-def call(String repoUrl, String branch = 'master') {
-
-    git branch: branch,
-        url: repoUrl
-
-    sh 'gitleaks detect --source . --no-git || true'
-    sh 'dependency-check.sh --scan . || true'
-}
